624517d05e7eed718061656ec75d210fb65b6eee
unknownMuse <elselwyn@gmail.com>
2022-03-30 10:41:57 +0100

    First commit

---
+++ lib/root.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CryptoApp extends StatelessWidget {
+  const CryptoApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
